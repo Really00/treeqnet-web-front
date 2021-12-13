5824023cff8ce6030233376f9715475672502bfe
Really00 <1213937855@qq.com>
2021-12-13 15:42:32 +0800

    m

--- src/pages/tasks/insert.vue
+++ src/pages/tasks/insert.vue
@@ -345,9 +345,10 @@ export default {
                       path: "/home/lzl/tumor/individual-center-web/upload/example.zip"
                   }
               };
+            path = "/home/lzl/tumor/individual-center-web/upload/example.zip"
              console.log(params)
               this.instance.download(params).then((res)=>{ 
-                this.downloadfile(res.data,path.value.split("/").pop());
+                this.downloadfile(res.data,path.split("/").pop());
               });         
     },
      downloadfile (data,fileName) {
